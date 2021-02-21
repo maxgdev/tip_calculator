91d090521bbb813b70ab027b2e146f0c2344bc72
maxgdev <max@maxgdev.net>
2021-02-21 16:44:02 +0000

    Added container for user input

--- lib/home.dart
+++ lib/home.dart
@@ -13,9 +13,7 @@ class _BillSplitterState extends State<BillSplitter> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        margin: EdgeInsets.only(
-          top: MediaQuery.of(context).size.height * 0.1
-        ),
+        margin: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.1),
         alignment: Alignment.center,
         color: Colors.white,
         child: ListView(
@@ -26,24 +24,39 @@ class _BillSplitterState extends State<BillSplitter> {
               width: 150,
               height: 150,
               decoration: BoxDecoration(
-                color: Colors.blue[100],
-                borderRadius: BorderRadius.circular(12.0)
+                  color: Colors.blue[100],
+                  borderRadius: BorderRadius.circular(12.0)),
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Total Per Person",
+                        style: TextStyle(fontSize: 20, color: Colors.blue)),
+                    Text(
+                      "£120",
+                      style: TextStyle(fontSize: 30, color: Colors.blue),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 20.0),
+              padding: EdgeInsets.all(12.0),
+              decoration: BoxDecoration(
+                color: Colors.transparent,
+                border: Border.all(
+                  color: Colors.blueGrey.shade100,
+                  style: BorderStyle.solid,
+                ),
               ),
               child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text("Total Per Person",
-                  style: TextStyle(fontSize: 20, color: Colors.blue)
-                  ),
-                  Text("£120",
-                  style: TextStyle(fontSize: 30, color: Colors.blue),)
-                ],
+                children: [],
               ),
-            )
+            ),
           ],
         ),
       ),
-
     );
   }
 }
